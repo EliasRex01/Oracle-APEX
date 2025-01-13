8a7a2899ead9218b982c5ee41dc60fc6c2a798fa
Rex Elias <56458391+EliasRex01@users.noreply.github.com>
2025-01-12 22:05:21 -0300

    Add files via upload

---
+++ Shared Components/Return a id using function.sql
@@ -0,0 +1,25 @@
+-- Application 229853 / Shared Components / Application Computations / Create/Edit
+-- Computation Item: PA_ID_PERSONA
+-- Computation Type: Function Body
+-- Language: PL/SQL
+-- Computation: return ctb_pkg_adm.fn_id_persona;
+
+-- Conditions -> Computation Type -> Never
+
+
+
+-- Type: Function Body
+-- Point: After Authentication
+
+
+
+----------------------------------------------------
+
+-- Application 229853 / Shared Components / Application Items / Create/Edit
+-- Scope: Application
+-- Protection Level: Restricted - May not be set from browser
+-- Computed On: AFTER_LOGIN
+
+-- Name: PA_ID_PERSONA
+-- Session State Protection: Restricted - May not set by browser
+-- Escape Special Characters -> valid
